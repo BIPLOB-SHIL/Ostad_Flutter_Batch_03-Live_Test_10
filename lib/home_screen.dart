a07313b3fe37c2029e05afc8bf32f0ea2febb5e5
BIPLOB-SHIL <biplobshil.duet@gmail.com>
2023-07-17 23:41:56 +0600

    Second commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -18,11 +18,236 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Profile"),
+          title: const Text("News feed"),
           centerTitle: false,
           elevation: 0,
         ),
-        body: Text("Hello")
+        body: OrientationBuilder(builder: (context,orientation){
+
+          if (orientation == Orientation.portrait){
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black12,
+                        alignment: Alignment.center,
+                        // where to position the child
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          child: Text("150 x 150"),
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black12,
+                        alignment: Alignment.center,
+                        // where to position the child
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          child: Text("150 x 150"),
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black12,
+                        alignment: Alignment.center,
+                        // where to position the child
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          child: Text("150 x 150"),
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black12,
+                        alignment: Alignment.center,
+                        // where to position the child
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          child: Text("150 x 150"),
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black12,
+                        alignment: Alignment.center,
+                        // where to position the child
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          child: Text("150 x 150"),
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
+                        color: Colors.black12,
+                        alignment: Alignment.center,
+                        // where to position the child
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          child: Text("150 x 150"),
+                          color: Colors.grey,
+                        ),
+                      ),
+
+                    ),
+                  ],
+                ),
+              ),
+            );
+
+
+          }else{
+            return Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.black12,
+                    alignment: Alignment.center,
+                    // where to position the child
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: Text("150 x 150"),
+                      color: Colors.grey,
+                    ),
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.black12,
+                    alignment: Alignment.center,
+                    // where to position the child
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: Text("150 x 150"),
+                      color: Colors.grey,
+                    ),
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.black12,
+                    alignment: Alignment.center,
+                    // where to position the child
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: Text("150 x 150"),
+                      color: Colors.grey,
+                    ),
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.black12,
+                    alignment: Alignment.center,
+                    // where to position the child
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: Text("150 x 150"),
+                      color: Colors.grey,
+                    ),
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.black12,
+                    alignment: Alignment.center,
+                    // where to position the child
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: Text("150 x 150"),
+                      color: Colors.grey,
+                    ),
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    color: Colors.black12,
+                    alignment: Alignment.center,
+                    // where to position the child
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      child: Text("150 x 150"),
+                      color: Colors.grey,
+                    ),
+                  ),
+
+                ),
+              ],
+            );
+          }
+        })
     );
   }
 }
